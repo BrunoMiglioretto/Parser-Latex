6ad6f82b48e10130285b193521fb4a4c7c79d1a7
Bruno Miglioretto <brunomiglioretto@gmail.com>
2025-04-24 00:17:40 -0300

    adjust patterns

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from enum import Enum
-
+from typing import Any
 
 ALPHABET = list(r"abcdefghijklmnopqrstuvwxyz0123456789\()")
 
@@ -16,7 +16,7 @@ class Lexeme(Enum):
 class DeterministicFiniteAutomata:
     def __init__(
         self,
-        transactions: dict[tuple[int, str] : int],
+        transactions: set[tuple[int, Any, int]],
         initial_state: int,
         final_states: set[int],
     ):
@@ -38,69 +38,81 @@ class DeterministicFiniteAutomata:
 
         while symbols:
             try:
-                state = self.transactions[(state, symbols.pop(0))]
+                for transaction in self.transactions:
+                    if transaction[0] == state and transaction[1](symbols.pop(0)):
+                        state = transaction[2]
+                        break
             except KeyError:
                 break
         return state in self.final_states
 
 
-def build_full_word_transactions(word: str) -> tuple[dict[tuple[int, str] : int], int]:
+def build_full_word_transactions(word: str) -> tuple[set[tuple[int, Any, int]], int]:
     state = 0
     final_state = state + 1
-    transactions = {}
+    transactions = []
     for symbol in word:
-        transactions[(state, symbol)] = final_state
+        transactions.append((state, lambda x: x == symbol, final_state))
         state += 1
         final_state += 1
 
-    return transactions, final_state
+    transactions.append((final_state, lambda x: True, final_state + 1))
+    return set(transactions), final_state
 
 
-def build_number_alphabet_transactions() -> dict[tuple[int, str] : int]:
-    valid_symbols = "abcdefghijklmnopqrstuvwxyz0123456789"
-    transactions = {}
-    for symbol in valid_symbols:
-        transactions[(0, symbol)] = 1
-        transactions[(1, symbol)] = 1
-    return transactions
-
-
-def build_list_dfa():
-    DFAs = {
-        Lexeme.CONSTANT: [],
-        Lexeme.UNARY_OPERATOR: [],
-        Lexeme.BINARY_OPERATOR: [],
-        Lexeme.PROPOSITION: [
+def build_patterns():
+    patterns = [
+        (
+            Lexeme.PROPOSITION,
             DeterministicFiniteAutomata(
-                transactions=build_number_alphabet_transactions(),
+                transactions={
+                    (0, lambda x: x in "abcdefghijklmnopqrstuvwxyz0123456789", 1),
+                    (1, lambda x: x not in "abcdefghijklmnopqrstuvwxyz0123456789", 2),
+                },
                 initial_state=0,
                 final_states={1},
             ),
-        ],
-        Lexeme.OPEN_PARENTHESIS: [
+        ),
+        (
+            Lexeme.OPEN_PARENTHESIS,
             DeterministicFiniteAutomata(
-                transactions={(0, "("): 1}, initial_state=0, final_states={1}
+                transactions={(0, lambda x: x == "(", 1), (1, lambda x: x != "(", 2)},
+                initial_state=0,
+                final_states={1},
             ),
-        ],
-        Lexeme.CLOSE_PARENTHESIS: [
+        ),
+        (
+            Lexeme.CLOSE_PARENTHESIS,
             DeterministicFiniteAutomata(
-                transactions={(0, ")"): 1}, initial_state=0, final_states={1}
+                transactions={(0, lambda x: x == ")", 1), (1, lambda x: x != ")", 2)},
+                initial_state=0,
+                final_states={1},
             ),
-        ],
-    }
+        ),
+    ]
     for word in ["true", "false"]:
         transactions, final_state = build_full_word_transactions(word)
-        DFAs[Lexeme.CONSTANT].append(
-            DeterministicFiniteAutomata(
-                transactions=transactions, initial_state=0, final_states={final_state}
+        patterns.append(
+            (
+                Lexeme.CONSTANT,
+                DeterministicFiniteAutomata(
+                    transactions=transactions,
+                    initial_state=0,
+                    final_states={final_state},
+                ),
             )
         )
 
     for word in [r"\neg"]:
         transactions, final_state = build_full_word_transactions(word)
-        DFAs[Lexeme.UNARY_OPERATOR].append(
-            DeterministicFiniteAutomata(
-                transactions=transactions, initial_state=0, final_states={final_state}
+        patterns.append(
+            (
+                Lexeme.UNARY_OPERATOR,
+                DeterministicFiniteAutomata(
+                    transactions=transactions,
+                    initial_state=0,
+                    final_states={final_state},
+                ),
             )
         )
 
@@ -111,38 +123,63 @@ def build_list_dfa():
         r"\leftrightarrow",
     ]:
         transactions, final_state = build_full_word_transactions(word)
-        DFAs[Lexeme.BINARY_OPERATOR].append(
-            DeterministicFiniteAutomata(
-                transactions=transactions, initial_state=0, final_states={final_state}
+        patterns.append(
+            (
+                Lexeme.BINARY_OPERATOR,
+                DeterministicFiniteAutomata(
+                    transactions=transactions,
+                    initial_state=0,
+                    final_states={final_state},
+                ),
             )
         )
 
-    return DFAs
+    return patterns
 
 
 class LexicalAnalyser:
     def __init__(self):
-        self.lexemes = build_list_dfa()
+        self.patterns = build_patterns()
 
     def execute(self, content: str) -> list[tuple[Lexeme, str]]:
         tokens: list[tuple[Lexeme, str]] = []
 
-        index = 1
         word = ""
         while content:
-            word += content[0]
-            content = content[1:]
+            possible_patterns = self.patterns
+            try:
+                while possible_patterns:
+                    last_word = word
+                    last_content = content
+                    lasts_patterns = possible_patterns
 
-            for lexeme, DFAs in self.lexemes.items():
-                for DFA in DFAs:
-                    if DFA.run(word):
-                        tokens.append((lexeme, word))
+                    word += content[0]
+                    content = content[1:]
+
+                    possible_patterns = self.execute_patterns(word, possible_patterns)
+                    if not possible_patterns:
+                        tokens.append((lasts_patterns[0][0], last_word))
                         word = ""
+                        content = last_content
+
+                    if not content:
+                        if possible_patterns:
+                            tokens.append((possible_patterns[0][0], word))
+            except IndexError:
+                pass
 
         return tokens
+
+    def execute_patterns(self, word: str, patterns) -> list[tuple[Lexeme, set]]:
+        successfully_patterns = []
+
+        for pattern in patterns:
+            if pattern[1].run(word):
+                successfully_patterns.append(pattern)
+
+        return successfully_patterns
 
 
 content = "(asdf)"
 analyser = LexicalAnalyser()
 print(analyser.execute(content))
-print(analyser.execute(content)[1][0] == Lexeme.CLOSE_PARENTHESIS)
